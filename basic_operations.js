a1437bd8d020b964f041d83db55a8600cb127c9d
Phu T. Van <pvan@fredhutch.org>
2019-08-30 08:02:39 -0700

    added basic_operations.js

---
+++ basic_operations.js
@@ -0,0 +1,14 @@
+// BASIC OPERATIONS
+
+// read in a .csv
+const csv = require('csv-parser');
+const fs = require('fs');
+
+fs.createReadStream('data.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    console.log(row);
+  })
+  .on('end', () => {
+    console.log('CSV file successfully processed');
+  });
